20fad6f405ce08a945d6f1d1232aad05cfb7e2eb
Gustavo Henrique da Luz <gustavohenriquedaluz619@gmail.com>
2025-12-31 07:08:56 -0300

    31/12/2025

--- Eclipse/Devdojo/viradoNoJiraya/maratona-java/src/academy/devdojo/maratonajava/Oexceotion/exception/test/A97ExceptionTest01.java
+++ Eclipse/Devdojo/viradoNoJiraya/maratona-java/src/academy/devdojo/maratonajava/Oexceotion/exception/test/A97ExceptionTest01.java
@@ -5,16 +5,18 @@ import java.io.IOException;
 
 public class A97ExceptionTest01 {
 	public static void main(String[] args) {
-		
+		criarNovoArquivo();
 	}
 	
 	private static void criarNovoArquivo() {
 		File file = new File("arquivo\\teste.txt");
 		try {
-			
+		boolean isCriado = file.createNewFile();			
+		System.out.println("Arquivo criado " + isCriado);
 		} catch (IOException e) {
+			e.printStackTrace();
 			
 		}
-		file.createNewFile();
+
 	}
 }
